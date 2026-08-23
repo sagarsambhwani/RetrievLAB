6dcc3ec3d674c2860c0c347f88dca69c8092d12a
Sagar Sambhwani <sagar.2001.a20@gmail.com>
2026-08-23 18:32:11 +0530

    feat(ranking): export ReciprocalRankFusion and reciprocal_rank_fusion

---
+++ src/retrievlab/ranking/__init__.py
@@ -0,0 +1,8 @@
+"""Ranking and rank aggregation algorithms for RetrievLab."""
+
+from retrievlab.ranking.fusion import ReciprocalRankFusion, reciprocal_rank_fusion
+
+__all__ = [
+    "ReciprocalRankFusion",
+    "reciprocal_rank_fusion",
+]
